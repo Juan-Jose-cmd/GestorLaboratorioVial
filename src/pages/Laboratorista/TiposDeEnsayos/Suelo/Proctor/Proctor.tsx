7373050c6e842c2db980a7c6ec0acf7c62de1f28
Juan Jose Pereyra <juanjopereyra0@gmail.com>
2025-11-26 10:33:16 -0300

    Creacion de interface para proctor

--- src/pages/Laboratorista/TiposDeEnsayos/Suelo/Proctor/Proctor.tsx
+++ src/pages/Laboratorista/TiposDeEnsayos/Suelo/Proctor/Proctor.tsx
@@ -1,3 +1,41 @@
+import './Proctor.css';
+
+interface EnsayoProctor {
+    obra: string;
+    muestraId: string;
+    clasificacion: string;
+    tipoProctor: 'Normal | Modificado';
+    norma: 'IRAM | ASTM';
+    fecha: string;
+    hora: string;
+    observaciones?: string;
+}
+
+interface EquipoProctor {
+    pesoMolde: number;
+    diametroMolde: number;
+    alturaMolde: number;
+    volumenMolde: number;
+    pesoPison: number;
+    alturaCaida: number;
+}
+
+interface PuntoProctor {
+    humedad: number;
+    moldeMasSuelo: number;
+    golpes: number;
+    observaciones?: string;
+    pesoSuelo: number;
+    densidadHumeda: number;
+    densidadSeca: number;
+}
+
+interface ProctorData {
+    general: EnsayoProctor;
+    equipo: EquipoProctor;
+    puntos: PuntoProctor[];
+}
+
 const Proctor = () => {
     return <h2>Proctor</h2>;
 };
